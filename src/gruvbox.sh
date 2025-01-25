549d446b047de022aba71f0d085f1e084948f61a
Paolo Bignardi <paolo.bigno@gmail.com>
2025-01-25 16:15:35 +0100

    added gruvbox theme to oneminimal

---
+++ src/gruvbox.sh
@@ -0,0 +1,24 @@
+tmux set-option -g @thm_black "#282828"
+tmux set-option -g @thm_bg "#1d2021"
+tmux set-option -g @thm_grey "#928374"
+tmux set-option -g @thm_fg "#f9f5d7"
+
+# neutral colors
+tmux set-option -g @thm_blue "#458588"
+tmux set-option -g @thm_cyan "#689d6a"
+tmux set-option -g @thm_red "#cc241d"
+tmux set-option -g @thm_orange "#d65d0e"
+tmux set-option -g @thm_yellow "#d79921"
+tmux set-option -g @thm_green "#98971a"
+tmux set-option -g @thm_purple "#b16286"
+
+# bright colors
+tmux set-option -g @thm_red      '#fb4934'
+tmux set-option -g @thm_green    '#b8bb26'
+tmux set-option -g @thm_yellow   '#fabd2f'
+tmux set-option -g @thm_blue     '#83a598'
+tmux set-option -g @thm_purple   '#d3869b'
+tmux set-option -g @thm_aqua     '#8ec07c'
+tmux set-option -g @thm_orange   '#fe8019'
+
+tmux set-option -g @thm_light_grey "#a89984"
