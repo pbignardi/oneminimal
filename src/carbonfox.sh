65482a943b8ec6157fea3c7e2cd560e69c1519b5
Paolo Bignardi <paolo.bigno@gmail.com>
2025-01-30 21:54:21 +0100

    bugfix: fixed color codes in all colorschemes

--- src/carbonfox.sh
+++ src/carbonfox.sh
@@ -1,13 +1,13 @@
 tmux set-option -g @thm_black "#000000"
 tmux set-option -g @thm_bg "#161616"
-tmux set-option -g @thm_grey "#7b7c7e"
-tmux set-option -g @thm_fg "#f2f4f8"
-tmux set-option -g @thm_red      '#ee5396'
-tmux set-option -g @thm_green    '#25be6a'
-tmux set-option -g @thm_yellow   '#3ddbd9'
-tmux set-option -g @thm_blue     '#78a9ff'
-tmux set-option -g @thm_purple   '#be95ff'
-tmux set-option -g @thm_aqua     '#08bdba'
-tmux set-option -g @thm_orange   '#ff7eb6'
-tmux set-option -g @thm_light_grey "#b6b8bb"
+tmux set-option -g @thm_grey "#7B7C7E"
+tmux set-option -g @thm_fg "#F2F4F8"
+tmux set-option -g @thm_red '#EE5396'
+tmux set-option -g @thm_green '#25BE6A'
+tmux set-option -g @thm_yellow '#3DDBD9'
+tmux set-option -g @thm_blue '#78A9FF'
+tmux set-option -g @thm_purple '#BE95FF'
+tmux set-option -g @thm_aqua '#08BDBA'
+tmux set-option -g @thm_orange '#FF7EB6'
+tmux set-option -g @thm_light_grey "#B6B8BB"
 
--- src/gruvbox.sh
+++ src/gruvbox.sh
@@ -1,24 +1,24 @@
 tmux set-option -g @thm_black "#282828"
-tmux set-option -g @thm_bg "#1d2021"
+tmux set-option -g @thm_bg "#1D2021"
 tmux set-option -g @thm_grey "#928374"
-tmux set-option -g @thm_fg "#f9f5d7"
+tmux set-option -g @thm_fg "#F9F5D7"
 
 # neutral colors
 tmux set-option -g @thm_blue "#458588"
-tmux set-option -g @thm_cyan "#689d6a"
-tmux set-option -g @thm_red "#cc241d"
-tmux set-option -g @thm_orange "#d65d0e"
-tmux set-option -g @thm_yellow "#d79921"
-tmux set-option -g @thm_green "#98971a"
-tmux set-option -g @thm_purple "#b16286"
+tmux set-option -g @thm_cyan "#689D6A"
+tmux set-option -g @thm_red "#CC241D"
+tmux set-option -g @thm_orange "#D65D0E"
+tmux set-option -g @thm_yellow "#D79921"
+tmux set-option -g @thm_green "#98971A"
+tmux set-option -g @thm_purple "#B16286"
 
 # bright colors
-tmux set-option -g @thm_red      '#fb4934'
-tmux set-option -g @thm_green    '#b8bb26'
-tmux set-option -g @thm_yellow   '#fabd2f'
-tmux set-option -g @thm_blue     '#83a598'
-tmux set-option -g @thm_purple   '#d3869b'
-tmux set-option -g @thm_aqua     '#8ec07c'
-tmux set-option -g @thm_orange   '#fe8019'
+tmux set-option -g @thm_red "#FB4934"
+tmux set-option -g @thm_green "#B8BB26"
+tmux set-option -g @thm_yellow "#FABD2F"
+tmux set-option -g @thm_blue "#83A598"
+tmux set-option -g @thm_purple "#D3869B"
+tmux set-option -g @thm_aqua "#8EC07C"
+tmux set-option -g @thm_orange "#FE8019"
 
-tmux set-option -g @thm_light_grey "#a89984"
+tmux set-option -g @thm_light_grey "#A89984"
